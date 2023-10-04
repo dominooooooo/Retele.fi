48001f18da0e5582f9813cb99b49952bfb12eb4b
Lauri <late033@gmail.com>
2023-10-04 18:35:56 +0300

    d

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div style={{ minHeight: '100vh', flexDirection: 'column', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
         <div style={{ flexDirection: 'row' }}>
-          <Image source={require('./retele.png')} style={{width: 400, height: 120}}/>
+          <Image source={require('./retele.png')} style={{ height: 100, width: 360 }}/>
         </div>
         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: 'calc(10px + 1vmin)', color: 'rgb(0, 0, 0)' }}>
           <Image source={require('./instagram.png')} style={{width: 20, height: 20}}/>
