69aa19a372c37c38f4c2cbacb7226598950a3ee3
Lauri <late033@gmail.com>
2023-10-04 17:18:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
       <div className="App-header">
         <div className="App-2">
-          <Image source={require('./retele.png')} style={{width: 400, height: 120}}/>
+          <Image source={require('./retele.png')} style={{width: 400, height: 120, padding: 60}}/>
         </div>
         <div className="App-3">
           <Image source={require('./instagram.png')} style={{width: 20, height: 20}}/>
