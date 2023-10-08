c29a33e9eb7178e7cd7d5f46677710e593e3b5a2
Lauri <late033@gmail.com>
2023-10-08 13:33:32 +0300

    infosite

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './index.css';
 import Home from './Home'
+import Info from './Info'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/tietoa' element={<Info />} />
       </Routes>
     </div>
   </Router>
--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
+import { Link } from "react-router-dom";
 import './App.css';
 
 const Home = () => {
   return (
       <div style={{ minHeight: '100vh', flexDirection: 'column', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
-        <div style={{ flexDirection: 'row' }}>
-          <Image source={require('./retele.png')} style={{ resizeMode: 'contain', height: 98, width: 360 }}/>
+        <div style={{ marginBottom: 10, display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: 'calc(10px + 1vmin)', fontFamily: 'Poppins, sans-serif' }}>
+          <Link className="info" to="/tietoa">
+            <p className="info">Mikä on Retele?</p>
+          </Link>
         </div>
 
-        {/* <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: 'calc(10px + 1vmin)', color: 'rgb(0, 0, 0)' }}>
-          <p style={{ }}>Ota sillä välin somet haltuun:</p>
-        </div> */}
+        <div style={{ flexDirection: 'row' }}>
+          <Image className="retelemain" source={require('./img/retele.png')} style={{ height: 120, width: 370 }}/>
+        </div>
 
         <div style={{ marginBottom: 12, display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: 'calc(10px + 1vmin)' }}>
-          <Image source={require('./instagram.png')} style={{ width: 20, height: 20 }}/>
+          <Image source={require('./img/instagram.png')} style={{ width: 20, height: 20 }}/>
           <p style={{ padding: 5, paddingRight: 10 }}>@retele_fi</p>
 
           <p style={{ paddingRight: 8 }}>•</p>
 
-          <Image source={require('./facebook.png')} style={{ width: 25, height: 25}}/>
+          <Image source={require('./img/facebook.png')} style={{ width: 25, height: 25}}/>
           <p style={{ padding: 3 }}>@Retele</p>
 
           <p style={{ paddingLeft: 7 }}>•</p>
@@ -28,16 +31,15 @@ const Home = () => {
         </div>
 
         <div style={{ marginTop: -40, display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: 'calc(10px + 1vmin)' }}>
-          <Image source={require('./tiktok.png')} style={{ width: 20, height: 20}}/>
+          <Image source={require('./img/tiktok.png')} style={{ width: 20, height: 20}}/>
           <p style={{ padding: 3, paddingRight: 10 }}>@retele_fi</p>
 
           <p style={{ paddingRight: 3 }}>•</p>
 
-          <Image source={require('./x.png')} style={{ width: 30, height: 30}}/>
+          <Image source={require('./img/x.png')} style={{ width: 30, height: 30}}/>
           <p>@retele_fi</p>
         </div>
-
-      </div>
+    </div>
   );
 }
 
