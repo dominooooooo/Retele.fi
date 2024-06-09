2231ac1459b0ac0d6b0387fdb8a2aa0fe3ea071b
dominooooooo <late033@gmail.com>
2024-06-09 21:11:24 +0300

    empty products

--- app/kauppa/page.jsx
+++ app/kauppa/page.jsx
@@ -5,7 +5,7 @@ import ProductCard from "@/components/productCard";
 import axios from "axios";
 import { Divider, Link } from "@nextui-org/react";
 import Image from "next/image";
-import AskForPhone from "@/components/AskForPhone"
+import AskForPhone from "@/components/AskForPhone";
 
 export default function Shop() {
   const [products, setProducts] = useState([]);
@@ -41,31 +41,38 @@ export default function Shop() {
   };
 
   const productsPerRow = 4;
-  const remainingSpots = productsPerRow - (products.length % productsPerRow);
+  const remainingSpots = products.length > 0 ? productsPerRow - (products.length % productsPerRow) : productsPerRow;
 
   return (
     <>
       <div className="font-black text-2xl text-center mt-12">KAUPPA</div>
-      <p className="text-center mt-2">Käteismaksu ja puhelin vaihdossa onnistuu. <Link href="/maksutavatjatoimitus" className="cursor-pointer">Lue lisää.</Link></p>
+      <p className="text-center mt-2">
+        Käteismaksu ja puhelin vaihdossa onnistuu.{" "}
+        <Link href="/maksutavatjatoimitus" className="cursor-pointer">Lue lisää.</Link>
+      </p>
       {isLoading ? (
         <div className="flex justify-center items-center mt-8">
           <Image unoptimized src="/loading.gif" alt="Loading" width={160} height={160} />
         </div>
       ) : (
         <div className="px-3 sm:px-0 grid grid-cols-2 sm:grid-cols-4 gap-3 sm:gap-5 max-w-[1040px] items-center mx-auto mt-8">
-          {products.map((product) => {
-            const productPrice = prices.find(
-              (price) => price.product === product.id
-            );
-            return (
-              <ProductCard
-                key={product.id}
-                product={product}
-                price={productPrice}
-              />
-            );
-          })}
-          {remainingSpots < productsPerRow && renderPlaceholders(remainingSpots)}
+          {products.length > 0 ? (
+            products.map((product) => {
+              const productPrice = prices.find(
+                (price) => price.product === product.id
+              );
+              return (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  price={productPrice}
+                />
+              );
+            })
+          ) : (
+            renderPlaceholders(productsPerRow)
+          )}
+          {products.length > 0 && remainingSpots < productsPerRow && renderPlaceholders(remainingSpots)}
         </div>
       )}
       <div className="flex justify-center items-center my-12 mx-auto max-w-xs">
